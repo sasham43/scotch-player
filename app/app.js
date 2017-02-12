5e04809f0d3dbff1be940a440b102427e4491c31
sasham43 <sashasemail@gmail.com>
2017-02-12 10:28:08 -0600

    add details component

--- app/app.js
+++ app/app.js
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Search from './components/search.component';
+import Details from './components/details.component';
 
 class App extends React.Component {
   render(){
     return (
-      <Search />
+      <div>
+        <Search />
+
+        <Details title={"Track title"} />
+      </div>
     );
   }
 }
